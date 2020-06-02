0f2041bd1a25d7aac0eaff87041d75c463508091
acharyarajasekhar <acharya.r@live.com>
2020-06-02 23:23:53 +0530

    profile card with photo viewer

--- projects/profile-card/src/lib/profile-card.module.ts
+++ projects/profile-card/src/lib/profile-card.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ProfileCardComponent } from './profile-card.component';
 import { CommonModule } from '@angular/common';
-
-
+import { PhotoViewerModule } from '@acharyarajasekhar/ion-native-services';
 
 @NgModule({
   declarations: [ProfileCardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    PhotoViewerModule 
   ],
   exports: [ProfileCardComponent]
 })
